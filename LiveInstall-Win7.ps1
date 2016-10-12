8bc3bb591ee5d4dc72b2711aa5ef6e49acff8542
rob89m <rob89m@icloud.com>
2016-10-13 02:29:48 +1100

    Create LiveInstall-Win7.ps1
    Added as alternative to "LiveInstall-IWR.ps1" to support Windows 7

---
+++ LiveInstall-Win7.ps1
@@ -0,0 +1,18 @@
+MkDir("C:\Temp\BoxStarterDownload")
+
+$url = "https://github.com/rob89m/BoxStarter_BasicBuild/archive/master.zip"
+$output = "C:\Temp\BoxStarterDownload\master.zip"
+$start_time = Get-Date
+
+(New-Object System.Net.WebClient).DownloadFile($url, $output)
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+
+
+$shell = New-Object -ComObject shell.application
+$zip = $shell.NameSpace($output)
+foreach ($item in $zip.items()) {
+  $shell.Namespace("C:\Temp").CopyHere($item)
+}
+
+
+C:\Temp\BoxStarter_BasicBuild-master\process1.ps1
