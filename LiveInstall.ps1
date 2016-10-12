a218d2e972fd9cfd25022a3f7b1f7a270f012977
rob89m <rob89m@icloud.com>
2016-10-13 01:50:46 +1100

    Create LiveInstall.ps1
    Can be used to trigger download, extraction and execution of script from GitHub

---
+++ LiveInstall.ps1
@@ -0,0 +1,13 @@
+MkDir("C:\Temp\BoxStarterDownload")
+
+iwr -Uri https://github.com/rob89m/BoxStarter_BasicBuild/archive/master.zip -OutFile C:\Temp\BoxStarterDownload\master.zip
+
+
+$shell = New-Object -ComObject shell.application
+$zip = $shell.NameSpace("C:\Temp\BoxStarterDownload\master.zip")
+foreach ($item in $zip.items()) {
+  $shell.Namespace("C:\Temp").CopyHere($item)
+}
+
+
+C:\Temp\BoxStarter_BasicBuild-master\process1.ps1
